8473d0efb7474d3de7e2ae03051f5d3b1297421f
Iker González Camacho <ikergc1989@gmail.com>
2023-05-10 21:13:43 +0200

    feat: read numeros.txt filter by number size and write to numerosSalida.txt

--- UF3 File management/TestFicheros/src/LeerScanner.java
+++ UF3 File management/TestFicheros/src/LeerScanner.java
@@ -1,25 +1,37 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class LeerScanner {
 
     public static void main(String[] args) {
 
+        System.out.println("Working directory = " + System.getProperty("user.dir"));
         File f = new File("numeros.txt");
+        File fSalida = new File("numerosSalida.txt");
 
-        try {
-            Scanner scanner = new Scanner(f);
+        try (Scanner scanner = new Scanner(f);
+             Scanner sc_usuario = new Scanner(System.in);
+             PrintWriter pw = new PrintWriter(fSalida);) {
+
             int num;
+            int numUsuario;
 
+            System.out.println("Introduce un numero");
+            numUsuario = sc_usuario.nextInt();
+
+            System.out.println("============");
+            System.out.println("Fichero");
             while(scanner.hasNext()) {
                 num = scanner.nextInt();
-                System.out.println(num);
+                if(num > numUsuario){
+                    pw.println(num);
+                }
             }
         } catch (FileNotFoundException fnfe) {
-            throw new RuntimeException(fnfe);
+            System.out.println(fnfe.getMessage());
         }
-
-
     }
 }
+
